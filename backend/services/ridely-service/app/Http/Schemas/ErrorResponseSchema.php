5b206c0cf2f052e0ea41aa22f0a2443ad008ef45
Anderson Contreira <anderson.contreira@gmail.com>
2025-07-28 18:24:34 -0300

    Added new schemas.

---
+++ backend/services/ridely-service/app/Http/Schemas/ErrorResponseSchema.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Schemas;
+
+/**
+ * @OA\Schema(
+ *     schema="ErrorResponse",
+ *     type="object",
+ *     @OA\Property(property="success", type="boolean", example=false),
+ *     @OA\Property(property="label", type="string", example="error"),
+ *     @OA\Property(property="code", type="integer", example=1),
+ *     @OA\Property(property="message", type="string", example="Erro ao buscar motoristas."),
+ *     @OA\Property(
+ *         property="detail",
+ *         type="array",
+ *         description="Stack trace (visível apenas em ambiente de desenvolvimento)",
+ *         @OA\Items(type="object")
+ *     )
+ * )
+ */
+class ErrorResponseSchema
+{
+
+}
